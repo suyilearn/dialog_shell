acb6a1e52ce4f2cebf627ef11b628ffc8926c0b5
lucasviola <lucasmatzenbacher@gmail.com>
2016-01-21 11:33:58 -0200

    Fixing showDetails parameter

--- ls_prompt.sh
+++ ls_prompt.sh
@@ -8,7 +8,7 @@ showDetails= ;
 dialog --yesno 'Use colors?' 0 0 && useColors='--color=yes'
 dalog --yesno 'Show hidden files?' 0 0 && showHiddenFiles='-a'
 dialog --yesno 'Include sub directories?' 0 0 && showSubDirs='-R'
-dialog --yesno 'Show details?' 0 0 && showDetails='-1'
+dialog --yesno 'Show details?' 0 0 && showDetails='-l'
 
 # Processing
 ls $useColors $showHiddenFiles $showSubDirs $showDetails
